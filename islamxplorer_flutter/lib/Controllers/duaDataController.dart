c58afbc6150727c07a5177073a8a446ddc02f1f5
DeveloperAhmadHassan <ahmad.hassantariq921@gmail.com>
2024-01-22 09:44:21 +0500

    "Code Improvements"

---
+++ islamxplorer_flutter/lib/Controllers/duaDataController.dart
@@ -0,0 +1,207 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:islamxplorer_flutter/Controllers/userDataController.dart';
+import 'package:islamxplorer_flutter/models/dua.dart';
+import 'package:islamxplorer_flutter/models/duaType.dart';
+import 'package:islamxplorer_flutter/models/hadith.dart';
+import 'package:islamxplorer_flutter/models/user.dart';
+import 'package:http/http.dart' as http;
+
+class DuaDataController{
+  Future<List<Dua>> fetchAllDuas() async {
+    const url = "http://192.168.56.1:48275/duas";
+    print("Hello1");
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonData = json.decode(response.body)['data'];
+        print("Hello2");
+        final List<Dua> duas = jsonData.map((data) => Dua.fromJson(data)).toList();
+        print("Hello3");
+        return duas;
+      }  else {
+        throw Exception('Failed to load Duas');
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+
+  Future<List<DuaType>> fetchAllDuaTypes() async {
+    const url = "http://192.168.56.1:48275/getAllDuaTypes";
+    print("Hello1");
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonData = json.decode(response.body)['data'];
+        print("Hello2");
+        final List<DuaType> duaTypes = jsonData.map((data) => DuaType.fromJson(data)).toList();
+        print("Hello3");
+        return duaTypes;
+      }  else {
+        throw Exception('Failed to load Duas');
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+
+  Future<List<Dua>> fetchDuasFromTypes(String type) async {
+    var url = "http://192.168.56.1:48275/duas?type=$type";
+    print("Hello1");
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonData = json.decode(response.body)['data'];
+        print("Hello2");
+        final List<Dua> duas = jsonData.map((data) => Dua.fromJson(data)).toList();
+        print("Hello3");
+        return duas;
+      }  else {
+        throw Exception('Failed to load Duas');
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+
+  Future<bool> addDua(Dua dua) async {
+    const url = "http://192.168.56.1:48275/duas";
+    print("Hello1");
+
+    final Map<String, dynamic> requestBody = {
+      "duaID": dua.id,
+      "englishText": dua.englishText,
+      "arabicText": dua.arabicText,
+      "title": dua.title,
+      "explanation": dua.explanation,
+      "transliteration": dua.transliteration,
+      "surah": dua.surah,
+      "verses": dua.verses,
+      "types": dua.types,
+    };
+
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(requestBody),
+      );
+
+      if (response.statusCode == 201) {
+        print("Dua added successfully");
+        return true;
+      } else {
+        print("Failed to add Dua. Status code: ${response.statusCode}");
+        return false;
+      }
+    } catch (e) {
+      print("Error: $e");
+      return false;
+    }
+  }
+
+  Future<Dua> getDuaByID(String id) async {
+    var url = "http://192.168.56.1:48275/duas?id=$id";
+    print("Hello1");
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonData = json.decode(response.body)['data'];
+        print("Hello2");
+        final List<Dua> duas = jsonData.map((data) => Dua.fromJson(data)).toList();
+        print("Hello3");
+
+        duas.first.updateBookmarkStatus(await setBookmark(duas.first.id));
+        duas.first.updateReportStatus(await setReport(duas.first.id));
+
+        return duas.first;
+      }  else {
+        throw Exception('Failed to load Dua');
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+
+  Future<bool> updateDua(Dua dua, String oldID) async {
+    String url = "http://192.168.56.1:48275/duas?id=$oldID";
+
+    try {
+      final response = await http.put(
+        Uri.parse(url),
+        body: jsonEncode({
+          "duaID": dua.id,
+          "englishText": dua.englishText,
+          "arabicText": dua.arabicText,
+          "title": dua.title,
+          "explanation": dua.explanation,
+          "transliteration": dua.transliteration,
+          "surah": dua.surah,
+          "verses": dua.verses,
+          "types": dua.types,
+        }),
+        headers: {"Content-Type": "application/json"},
+      );
+
+      if (response.statusCode == 200) {
+        print("Dua updated successfully");
+        return true;
+      } else {
+        throw Exception("Failed to update Dua");
+        return true;
+      }
+    } catch (e) {
+      print("Error updating Dua: $e");
+      throw Exception("Error updating Dua: $e");
+      return true;
+    }
+  }
+
+  Future<bool> deleteDua(String duaID) async {
+    String url = "http://192.168.56.1:48275/duas?id=$duaID";
+
+    try {
+      final response = await http.delete(
+        Uri.parse(url),
+        headers: {"Content-Type": "application/json"},
+      );
+
+      if (response.statusCode == 200) {
+        print("Dua deleted successfully");
+        return true;
+      } else {
+        throw Exception("Failed to delete Dua");
+        return true;
+      }
+    } catch (e) {
+      print("Error deleting Dua: $e");
+      throw Exception("Error deleting Dua: $e");
+      return true;
+    }
+  }
+
+  Future<bool> setBookmark(String id) async{
+    UserDataController userDataController = UserDataController();
+    if(await userDataController.isBookmarked(id)){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> setReport(String id) async{
+    UserDataController userDataController = UserDataController();
+    if(await userDataController.isReported(id)){
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
---
+++ islamxplorer_flutter/lib/Controllers/userDataController.dart
@@ -0,0 +1,196 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:islamxplorer_flutter/models/user.dart';
+
+class UserDataController{
+  Future<void> addUserToFirestore(AppUser user) async {
+    try {
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+      CollectionReference users = firestore.collection('Users');
+
+      await users.doc(user.uid).set({
+        'email': user.email,
+        'userName': user.userName,
+        'phone': user.phone,
+        'gender': user.gender,
+        'birthdate': user.birthdate
+      });
+
+      print('User updated!');
+    } catch (e) {
+      print('Error updating user to Firestore: $e');
+    }
+  }
+
+  Future<AppUser> getUserData() async {
+    var user = FirebaseAuth.instance.currentUser;
+    String? userId = user?.uid;
+    AppUser appUser = AppUser();
+    try {
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+      CollectionReference users = firestore.collection('Users');
+
+      DocumentSnapshot userDoc = await users.doc(userId).get();
+
+      if (userDoc.exists) {
+        Map<String, dynamic> userData = userDoc.data() as Map<String, dynamic>;
+
+        print('User ID: $userId');
+        print('Email: ${userData['email']}');
+        print('Username: ${userData['userName']}');
+        print('Type: ${userData['type']}');
+
+        appUser = AppUser(uid: userId,
+            userName: userData['userName'],
+            email: userData['email'],
+            birthdate: userData['birthdate'],
+            phone: userData['phone'],
+            type: userData['type']??"U",
+            gender: userData['gender']);
+
+        return appUser;
+
+      } else {
+        print('User with ID $userId does not exist in Firestore.');
+        return appUser;
+      }
+    } catch (e) {
+      print('Error fetching user data from Firestore: $e');
+      return appUser;
+    }
+  }
+
+  Future<bool> addBookmark(String id) async {
+    try {
+      var user = FirebaseAuth.instance.currentUser;
+
+      DocumentReference userDoc = FirebaseFirestore.instance.collection('Users').doc(user?.uid);
+      List<dynamic> currentBookmarks = (await userDoc.get()).get('bookmarks') ?? [];
+
+      currentBookmarks.add(id);
+
+      await userDoc.update({
+        'bookmarks': currentBookmarks,
+      });
+
+      print('Dua added to Bookmarks!');
+      return true;
+    } catch (e) {
+      print('Error adding bookmark to Firestore: $e');
+      return false;
+    }
+  }
+
+  Future<bool> removeBookmark(String id) async {
+    try {
+      var user = FirebaseAuth.instance.currentUser;
+
+      DocumentReference userDoc = FirebaseFirestore.instance.collection('Users').doc(user?.uid);
+      List<dynamic> currentBookmarks = (await userDoc.get()).get('bookmarks') ?? [];
+
+      currentBookmarks.remove(id);
+
+      await userDoc.update({
+        'bookmarks': currentBookmarks,
+      });
+
+      print('Dua removed from Bookmarks!');
+      return true;
+    } catch (e) {
+      print('Error removing bookmark from Firestore: $e');
+      return false;
+    }
+  }
+
+  Future<bool> isBookmarked(String id) async {
+    try {
+      var user = FirebaseAuth.instance.currentUser;
+
+      DocumentReference userDoc = FirebaseFirestore.instance.collection('Users').doc(user?.uid);
+      List<dynamic> currentBookmarks = (await userDoc.get()).get('bookmarks') ?? [];
+
+      return currentBookmarks.contains(id);
+    } catch (e) {
+      print('Error checking if bookmark exists in Firestore: $e');
+      return false;
+    }
+  }
+
+  Future<bool> addReport(String id, String message) async {
+    try {
+      var user = FirebaseAuth.instance.currentUser;
+
+      if (user != null) {
+        var reportsCollection = FirebaseFirestore.instance.collection('Reports');
+        var timestamp = DateTime.now().toUtc();
+
+        var newReportDocumentRef = reportsCollection.doc();
+        await newReportDocumentRef.set({
+          'userId': user.uid,
+          'reportedItemID': id,
+          'reportedItemType': message,
+          'reportedTime': timestamp,
+          'reportedMessage': message
+        });
+
+        var userDocumentRef = FirebaseFirestore.instance.collection('Users').doc(user.uid);
+        await userDocumentRef.update({
+          'reports': FieldValue.arrayUnion([{ 'reportID': newReportDocumentRef.id, 'itemID': id }])
+        });
+
+        print('Randomly generated document ID: ${newReportDocumentRef.id}');
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<bool> removeReport(String id) async {
+    try {
+      var user = FirebaseAuth.instance.currentUser;
+
+      DocumentReference userDoc = FirebaseFirestore.instance.collection('Users').doc(user?.uid);
+      List<dynamic> currentReports = (await userDoc.get()).get('reports') ?? [];
+
+      var matchingReport = currentReports.firstWhere((report) => report['itemID'] == id, orElse: () => null);
+
+      if (matchingReport != null) {
+        var reportID = matchingReport['reportID'];
+        currentReports.removeWhere((report) => report['itemID'] == id);
+        await userDoc.update({
+          'reports': currentReports,
+        });
+
+        var reportsCollection = FirebaseFirestore.instance.collection('Reports');
+        await reportsCollection.doc(reportID).delete();
+
+        print('Item removed from Reports!');
+        return true;
+      }
+      else{
+        return false;
+      }
+    }catch (e) {
+      print('Error removing bookmark from Firestore: $e');
+      return false;
+    }
+  }
+
+  Future<bool> isReported(String id) async {
+    try {
+      var user = FirebaseAuth.instance.currentUser;
+
+      DocumentReference userDoc = FirebaseFirestore.instance.collection('Users').doc(user?.uid);
+      List<dynamic> currentReports = (await userDoc.get()).get('reports') ?? [];
+
+      return currentReports.any((report) => report['itemID'] == id);
+    } catch (e) {
+      print('Error checking if report exists in Firestore: $e');
+      return false;
+    }
+  }
+}
