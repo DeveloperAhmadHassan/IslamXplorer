ebe6c5bcee26f7758d6e2b289dfc350e5fd955be
DeveloperAhmadHassan <ahmad.hassantariq921@gmail.com>
2024-02-29 10:17:59 +0500

    "Upgrade Flutter Version (3.19.2)"

--- islamxplorer_flutter/lib/pages/ReportListPage.dart
+++ islamxplorer_flutter/lib/pages/ReportListPage.dart
@@ -16,6 +16,10 @@ import 'package:islamxplorer_flutter/widgets/utils/custom_button.dart';
 import 'package:islamxplorer_flutter/widgets/utils/custom_error_widget.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
+import 'dart:math';
+import 'package:fl_chart/fl_chart.dart';
+
+
 import '../models/report.dart';
 
 class ReportListPage extends StatefulWidget {
@@ -29,6 +33,20 @@ class _ReportListPageState extends State<ReportListPage> {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+
+    final List<FlSpot> dummyData1 = List.generate(8, (index) {
+      return FlSpot(index.toDouble(), index * Random().nextDouble());
+    });
+
+    // This will be used to draw the orange line
+    final List<FlSpot> dummyData2 = List.generate(8, (index) {
+      return FlSpot(index.toDouble(), index * Random().nextDouble());
+    });
+
+    // This will be used to draw the blue line
+    final List<FlSpot> dummyData3 = List.generate(8, (index) {
+      return FlSpot(index.toDouble(), index * Random().nextDouble());
+    });
 
     ReportDataController reportDataController = ReportDataController();
     // Future<List<Report>> reports = reportDataController.getAllReports();
@@ -57,9 +75,37 @@ class _ReportListPageState extends State<ReportListPage> {
                   margin: EdgeInsets.all(10),
                   height: 120,
                   width: 350,
+                  child: LineChart(
+                    LineChartData(
+                      borderData: FlBorderData(show: false),
+                      lineBarsData: [
+                        // The red line
+                        LineChartBarData(
+                          spots: dummyData1,
+                          isCurved: true,
+                          barWidth: 3,
+                          color: Colors.indigo,
+                        ),
+                        // The orange line
+                        LineChartBarData(
+                          spots: dummyData2,
+                          isCurved: true,
+                          barWidth: 3,
+                          color: Colors.red,
+                        ),
+                        // The blue line
+                        LineChartBarData(
+                          spots: dummyData3,
+                          isCurved: false,
+                          barWidth: 3,
+                          color: Colors.blue,
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
               Row(
@@ -87,52 +133,21 @@ class _ReportListPageState extends State<ReportListPage> {
                   )
                 ],
               ),
-              SizedBox(
-                height: 20,
+              const SizedBox(
+                height: 40,
               ),
-              FutureBuilder<List<Report>>(
-                future: reportDataController.fetchAllReports(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  else if (snapshot.hasError) {
-                    return Center(
-                      child: Text('Error: ${snapshot}'),
-                    );
-                  }
-                  else {
-                    final List<Report> reports = snapshot.data ?? [];
-                    return SingleChildScrollView(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            // CustomButton("Add Verse", ()=>Navigator.push(context, MaterialPageRoute(builder: (cntext)=>AddUpdateVersePage()))),
-                          Card(
-                          elevation: 15,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)
-                          ),
-                          child: Container(
-                            margin: EdgeInsets.all(10),
-                            height: 80,
-                            width: 350,
-                            child: Text(reports[0].rid, style: TextStyle(
-                              color: Colors.red
-                            )),
-                          ),
-                        ),
-                          ],
-                        ),
-                      ),
-                    );
-                  }
-                },
+              Card(
+                elevation: 15,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)
+                ),
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  height: 80,
+                  width: 350,
+                ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
               Card(
@@ -146,7 +161,7 @@ class _ReportListPageState extends State<ReportListPage> {
                   width: 350,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
               Card(
@@ -160,21 +175,7 @@ class _ReportListPageState extends State<ReportListPage> {
                   width: 350,
                 ),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Card(
-                elevation: 15,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)
-                ),
-                child: Container(
-                  margin: EdgeInsets.all(10),
-                  height: 80,
-                  width: 350,
-                ),
-              ),
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
               Card(
@@ -195,93 +196,3 @@ class _ReportListPageState extends State<ReportListPage> {
     );
   }
 }
-//
-// class Cards extends StatelessWidget {
-//   final List<Report> reports;
-//
-//   const Cards({Key? key, required this.reports}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         for (var report in reports)
-//           ReportCard(report: report),
-//       ],
-//     );
-//   }
-// }
-//
-// class ReportCard extends StatelessWidget {
-//   final Report report;
-//
-//   const ReportCard({Key? key, required this.report}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         GestureDetector(
-//           // onTap: () => Navigator.push(
-//           //   context,
-//           //   MaterialPageRoute(
-//           //     builder: (context) => SearchItemPage(searchResultItem: verse),
-//           //   ),
-//           // ),
-//           child: Card(
-//             elevation: 7,
-//             color: HexColor.fromHexStr(AppColor.secondaryThemeSwatch1),
-//             child: ListTile(
-//               leading: Text(report.rid, style: TextStyle(
-//                   fontSize: 16,
-//                   fontWeight: FontWeight.w600
-//               )),
-//               title: Text(report.surah, style: TextStyle(
-//                   fontSize: 20,
-//                   fontWeight: FontWeight.w600
-//               )),
-//               subtitle: Text(verse.englishText, overflow: TextOverflow.ellipsis, style: TextStyle(
-//                   fontSize: 18,
-//                   fontWeight: FontWeight.w600
-//               )),
-//               trailing: Row(
-//                 mainAxisSize: MainAxisSize.min,
-//                 children: [
-//                   GestureDetector(
-//                     onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>AddUpdateVersePage(verse: verse, isUpdate: true,))),
-//                     child: Container(
-//                       padding: EdgeInsets.all(7),
-//                       decoration: BoxDecoration(
-//                           color: Colors.blue.withOpacity(0.3),
-//                           borderRadius: BorderRadius.all(Radius.circular(100))
-//                       ),
-//                       child: Icon(Icons.update),
-//                     ),
-//                   ),
-//                   SizedBox(width: 10,),
-//                   GestureDetector(
-//                     onTap: ()=>{deleteVerse(verse.id)},
-//                     child: Container(
-//                       padding: EdgeInsets.all(7),
-//                       decoration: BoxDecoration(
-//                           color: Colors.red.withOpacity(0.3),
-//                           borderRadius: BorderRadius.all(Radius.circular(100))
-//                       ),
-//                       child: Icon(Icons.delete),
-//                     ),
-//                   )
-//                 ],
-//               ),
-//             ),
-//           ),
-//         ),
-//         SizedBox(height: 20,)
-//       ],
-//     );
-//   }
-//
-//   void deleteVerse(String id){
-//     VerseDataController verseDataController = VerseDataController();
-//     verseDataController.deleteVerse(id);
-//   }
-// }
